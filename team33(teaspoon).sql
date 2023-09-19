58425166c3af0e36981c2d0aa14a293fea8aaa42
emsseme <bk75121@gmail.com>
2023-09-19 13:28:24 +0900

    [bo 230919] 이벤트 추첨 기능 구현

--- team33(teaspoon).sql
+++ team33(teaspoon).sql
@@ -1,12 +1,11 @@
 CREATE DATABASE team33;
-
 USE team33;
 
 SHOW TABLES;
 
--- 회원
+-- 회원 테이블
 CREATE TABLE MEMBER(
-	id VARCHAR(20) NOT NULL PRIMARY KEY,
+	id VARCHAR(20) PRIMARY KEY NOT NULL,
 	pw VARCHAR(300) NOT NULL,
 	NAME VARCHAR(50),
 	email VARCHAR(100) NOT NULL,
@@ -14,10 +13,12 @@ CREATE TABLE MEMBER(
 	addr1 VARCHAR(200),
 	addr2 VARCHAR(100),
 	postcode VARCHAR(10),
-	regdate DATETIME default CURRENT_TIMESTAMP,
+	regdate DATETIME DEFAULT CURRENT_TIMESTAMP(),
 	birth DATE,
 	pt INT(11) DEFAULT 0,
 	vistied INT(11) DEFAULT 0);
+
+SELECT * FROM member;
 
 -- 회원 테이블 더미데이터
 INSERT INTO MEMBER
@@ -28,9 +29,7 @@ VALUES('shin', '1234', '신이름', 'shin@edu.com', '01032146578', NULL, NULL, N
 UPDATE MEMBER SET pw='$2a$10$piyWPHz4GuwW0GxHZZfy1ORWtzKu7KPr9M0mFpw90hQJRQditQqJO' WHERE id = 'admin';
 UPDATE MEMBER SET pw='$2a$10$RZPVsTktT7RmMp7Qoj9pd.xGfZmsvYgsnbalImORzqx5hRNIoqPnm' WHERE id = 'shin';
 
-SELECT * FROM member;
-
-
+SELECT * FROM MEMBER;
 -- 커뮤니티 카테고리 테이블 생성
 CREATE TABLE category(
 	cate VARCHAR(5) PRIMARY KEY NOT NULL,
@@ -43,12 +42,13 @@ INSERT INTO category VALUES('B', '교육정보');
 INSERT INTO category VALUES('C', '교재/학원 추천');
 INSERT INTO category VALUES('D', '진로상담');
 
+
 -- 커뮤니티 게시판 테이블 생성
 CREATE TABLE community(
     cno INT PRIMARY KEY AUTO_INCREMENT,
     cate VARCHAR(5) NOT NULL,
     title VARCHAR(200) NOT NULL,
-    content VARCHAR(1500),
+    content VARCHAR(1000),
     author VARCHAR(20),
     resdate DATETIME DEFAULT CURRENT_TIMESTAMP(),
     cnt INT DEFAULT 0,
@@ -86,15 +86,7 @@ CREATE TABLE COMMENT(
 	FOREIGN KEY(author) REFERENCES member(id) ON DELETE CASCADE
 );
 
-SELECT * FROM comment;
-
-CREATE TABLE filterWord(
-	fno INT PRIMARY KEY AUTO_INCREMENT,
-	word VARCHAR(100) NOT NULL
-);
-
-select * from filterword;
-
+SELECT * FROM COMMENT;
 
 /* 공지사항 테이블 생성 */
 CREATE TABLE notice (
@@ -113,13 +105,13 @@ INSERT INTO notice VALUES (DEFAULT,'샘플 글 제목4  입니다.','여기는 �
 INSERT INTO notice VALUES (DEFAULT,'샘플 글 제목5  입니다.','여기는 샘플 글 5의 내용입니다.','admin',DEFAULT, DEFAULT);
 INSERT INTO notice VALUES (DEFAULT,'샘플 글 제목6  입니다.','여기는 샘플 글 6의 내용입니다.','admin',DEFAULT, DEFAULT);
 
-
 CREATE TABLE faq (
    fno INT  PRIMARY KEY AUTO_INCREMENT ,
    question VARCHAR(1000) NOT NULL,
    answer VARCHAR(1000) NOT NULL,
    cnt INT DEFAULT 0 NOT NULL
 );
+
 INSERT INTO faq(question, answer) VALUES('자주 묻는 질문입니다1', '답변입니다1');
 INSERT INTO faq(question, answer) VALUES('자주 묻는 질문입니다2', '답변입니다2');
 INSERT INTO faq(question, answer) VALUES('자주 묻는 질문입니다3', '답변입니다3');
@@ -130,19 +122,6 @@ INSERT INTO faq(question, answer) VALUES('자주 묻는 질문입니다7', '답�
 INSERT INTO faq(question, answer) VALUES('자주 묻는 질문입니다8', '답변입니다8');
 INSERT INTO faq(question, answer) VALUES('자주 묻는 질문입니다9', '답변입니다9');
 INSERT INTO faq(question, answer) VALUES('자주 묻는 질문입니다10', '답변입니다10');
-	
-	
--- qna
-CREATE TABLE qna(qno int PRIMARY KEY AUTO_INCREMENT,   		-- 번호
-   title VARCHAR(100) NOT NULL,   									-- 제목
-   content VARCHAR(1000) NOT NULL,   								-- 내용
-   author VARCHAR(16),   												-- 작성자
-   resdate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,    			-- 등록일
-   visited INT DEFAULT 0,   												-- 조회수
-   lev INT DEFAULT 0, 													-- 질문(0), 답변(1)
-   par INT,																	-- 질문(자신 레코드의 qno), 답변(질문의 글번호)
-   FOREIGN KEY(author) REFERENCES member(id) ON DELETE CASCADE
-);
 
 CREATE TABLE fileInfo(
   no int NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -159,16 +138,29 @@ CREATE TABLE fileboard (
   regdate timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
--- 출석체크 테이블 생성
-CREATE TABLE attendance (
-	ano INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	id VARCHAR(20),
-	attend DATE DEFAULT current_date
+-- QNA
+CREATE TABLE qna(
+                    qno int PRIMARY KEY AUTO_INCREMENT,   			-- 번호
+                    title VARCHAR(100) NOT NULL,   					-- 제목
+                    content VARCHAR(1000) NOT NULL,   				-- 내용
+                    author VARCHAR(16),   								-- 작성자
+                    resdate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,  -- 등록일
+                    lev INT DEFAULT 0, 									-- 질문(0), 답변(1)
+                    par INT,													-- 질문(자신 레코드의 qno), 답변(질문의 글번호)
+                    FOREIGN KEY(author) REFERENCES member(id) ON DELETE CASCADE
 );
 
-SELECT * FROM attendance;
+INSERT INTO	qna VALUES(DEFAULT, '질문1','질문1내용','admin',DEFAULT, DEFAULT, DEFAULT,1);
+INSERT INTO	qna VALUES(DEFAULT, '답변1','답변1내용','admin',DEFAULT, DEFAULT, 1,1);
 
+SELECT * FROM qna;
 
+UPDATE qna SET author='admin' WHERE qno=8;
+select qno, title, author, resdate from qna q join member m on(q.author=m.id) where par in (select par from qna group by par having count(par) < 2);
+
+SELECT COUNT(*) from qna q join member m on(q.author=m.id) where par in (select par from qna group by par having count(par) < 2);
+
+--이벤트글
 CREATE TABLE event (
 	eno int  PRIMARY KEY AUTO_INCREMENT,
 	title VARCHAR(100) NOT NULL,
@@ -181,16 +173,66 @@ CREATE TABLE event (
 	cnt INT DEFAULT 0 NOT NULL
 );
 
-SELECT * FROM event;
+SELECT * FROM EVENT;
 
+INSERT INTO EVENT VALUES(DEFAULT, '이벤트1','이벤트1내용', 1, 20230917,20230918,'admin',DEFAULT, DEFAULT);
+INSERT INTO EVENT VALUES(DEFAULT, '이벤트2','이벤트2내용', 1, 20230917,20230918,'admin',DEFAULT, DEFAULT);
+
+-- 회원의 이벤트 접수
 create table apply(
-	appno int AUTO_INCREMENT PRIMARY KEY,		
-	eno int not NULL,								
-	id varchar(100) not NULL,						
-	name varchar(100) not null,
-	tel varchar(13),			
+	appno int AUTO_INCREMENT PRIMARY KEY,		--접수 번호
+	eno int not NULL,									--이벤트글 번호
+	id varchar(100) not NULL,						--당첨자 아이디
+	name varchar(100) not null,					--당첨자 이름
+	tel varchar(13),									--전화번호	
 	foreign key(eno) references event(eno) on delete cascade,
 	foreign key(id) references member(id) on delete cascade);
 	
-SELECT * FROM apply;
+	SELECT * FROM apply;
+	select * from apply where eno=1;
+	
+INSERT INTO apply VALUES(DEFAULT, 1, 'kimbk111', '김보경', '01011111111');
+INSERT INTO apply VALUES(DEFAULT, 1, 'kimbk222', '김보경', '01022222222');
+INSERT INTO apply VALUES(DEFAULT, 1, 'kimbk333', '김보경', '01033333333');
+INSERT INTO apply VALUES(DEFAULT, 1, 'kimbk444', '김보경', '01044444444');
+INSERT INTO apply VALUES(DEFAULT, 1, 'kimbk555', '김보경', '01055555555');
+INSERT INTO apply VALUES(DEFAULT, 1, 'kimbk666', '김보경', '01066666666');
 
+INSERT INTO apply VALUES(DEFAULT, 2, 'kimbk111', '김보경', '01011111111');
+INSERT INTO apply VALUES(DEFAULT, 2, 'kimbk222', '김보경', '01022222222');
+INSERT INTO apply VALUES(DEFAULT, 2, 'kimbk333', '김보경', '01033333333');
+INSERT INTO apply VALUES(DEFAULT, 2, 'kimbk444', '김보경', '01044444444');
+INSERT INTO apply VALUES(DEFAULT, 2, 'kimbk555', '김보경', '01055555555');
+INSERT INTO apply VALUES(DEFAULT, 2, 'kimbk666', '김보경', '01066666666');
+
+
+-- 당첨자 리스트
+create table winnerList(
+	appno int auto-increment primary key not null,			--접수 번호
+	eno int not NULL,													--이벤트글 번호
+	id varchar(100) not NULL,										--당첨자 아이디
+	name varchar(100) not NULL,									--당첨자 이름
+	tel varchar(13),													--전화번호	
+	foreign key(eno) references event(eno) on delete cascade,
+	foreign key(id) references member(id) on delete cascade);
+	
+
+insert into winnerList select * from apply where eno=2 AND eno NOT IN (SELECT distinct eno FROM winnerList) order by rand() limit 5;
+
+select * from apply where eno=2 AND eno NOT IN (SELECT distinct eno FROM winnerList) order by rand() limit 5;
+
+
+SELECT * FROM winnerList;
+
+
+DELETE FROM winnerList WHERE appno=14;
+
+--당첨자 발표 글
+create table winner(
+	wno int primary key auto_increment,			--당첨글 번호
+	eno int not NULL,									--이벤트 글 번호
+	title varchar(100),								--글 제목
+	content varchar(1000),							--글 내용	
+	author varchar(100),								--작성자
+	resdate datetime default current_timestamp,	--작성일
+	foreign key(eno) references event(eno));
